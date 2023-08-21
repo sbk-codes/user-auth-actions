01b1b3655d2465d9e915129dc21ce42767066e4e
suresh <bist.suresh@ymailcom>
2023-08-21 22:38:05 +0530

    Feat/App: Updated logic

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :check_required_params, only: %i[signup show update close]
-  before_action :set_user, only: %i[show update close]
+  before_action :check_required_params, only: %i[signup show update]
+  before_action :set_user, only: %i[show update]
 
   def signup
     user = User.new(user_params)
@@ -13,25 +13,35 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
-    if authorized_user?
-      render_user_response('User details by user_id', @user)
+    requested_user = User.find_by(user_id: params[:user_id])
+
+    if requested_user
+      if authorized_user?
+        render_user_response('User details by user_id', requested_user)
+      else
+        render_unauthorized_response('Authentication Failed')
+      end
+    else
+      render_not_found_response('No User found')
+    end
+  end
+
+  def update
+    if authorized_user? && @auth_user == @user
+      @user.update(user_update_params)
+      render_user_response('User successfully updated', @user) && return
+    end
+
+    if @auth_user != @user && !@auth_user.nil?
+      render_forbidden_response('No Permission for Update')
     else
       render_unauthorized_response('Authentication Failed')
     end
   end
 
-  def update
-    if authorized_user?(@user)
-      @user.update(user_update_params)
-      render_user_response('User successfully updated', @user)
-    else
-      render_forbidden_response('No Permission for Update')
-    end
-  end
-
   def close
-    if authorized_user?(@user)
-      @user.destroy
+    if authorized_user?
+      @auth_user.destroy
       render_success_response('Account and user successfully removed')
     else
       render_unauthorized_response('Authentication Failed')
@@ -44,16 +54,17 @@ class Api::V1::UsersController < ApplicationController
     case action_name.to_sym
     when :signup
       if params[:user_id].blank? || params[:password].blank?
-        render_bad_request_response('required user_id and password')
+        render_error_response('Account creation failed', 'required user_id and password')
+        # render_bad_request_response('required user_id and password')
       end
-    when :show, :update, :close
+    when :show, :update
       render_bad_request_response('user_id is required') if params[:user_id].blank?
     end
   end
 
   def set_user
     @user = User.find_by(user_id: params[:user_id])
-    render_not_found_response('No User found') unless @user
+    render_unauthorized_response('Authentication Failed') unless @user
   end
 
   def user_params
@@ -105,10 +116,11 @@ class Api::V1::UsersController < ApplicationController
     }, status: :bad_request
   end
 
-  def authorized_user?(user = nil)
-    user ||= @user
-    authenticate_with_http_basic do |username, password|
-      user.user_id == username && user.password == password
-    end
+  def authorized_user?
+    user_id, password = ActionController::HttpAuthentication::Basic.user_name_and_password(request)
+    @auth_user = User.find_by(user_id: user_id)
+    return false unless @auth_user
+
+    @auth_user.password == password
   end
 end
